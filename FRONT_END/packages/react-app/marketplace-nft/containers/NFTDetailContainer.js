5703d5af4fcf5a331d31ae2556754d2e43dcab45
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2024-05-08 18:11:40 +0300

    Delete FRONT_END/packages/react-app/marketplace-nft/containers/NFTDetailContainer.js

--- FRONT_END/packages/react-app/marketplace-nft/containers/NFTDetailContainer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import NFTDetail from '../components/NFTDetail';
-
-const NFTDetailContainer = ({ match }) => {
-  const nftId = match.params.id;
-  const [nft, setNFT] = useState({});
-
-  useEffect(() => {
-    const fetchNFT =
