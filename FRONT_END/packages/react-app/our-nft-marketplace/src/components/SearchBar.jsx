0c9c89af4c32e5b7b3b93543dd9ed0a47c69276e
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2024-05-09 07:18:09 +0000

    Our-nft-frontend

---
+++ FRONT_END/packages/react-app/our-nft-marketplace/src/components/SearchBar.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import SearchIcon from '@mui/icons-material/Search';
+
+function SearchBar({ onSearch }) {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSearch(searchTerm); // Pass search term to parent component
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField
+        label="Search Properties"
+        variant="outlined"
+        value={searchTerm}
+        onChange={handleSearchChange}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+    </form>
+  );
+}
+
+export default SearchBar;
